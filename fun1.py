b0d5db34653a4feb559d6c3d9c4f3aeb54c0eb97
gilady19-meet <gilady19@meet.mit.edu>
2017-07-27 12:29:45 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,17 @@
+c = 0
+for number in range (1, 100+1):
+    print (number)
+    c = c + number
+print (c)
+
+#This function printed all the nembers from 1 to 100 and the sum
+
+def add_numbers():
+
+    for number in range (1, 100+1):
+        print (number)
+        c = c + number
+
+    return (answer)
+    
+    
